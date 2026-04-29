3297f453c01ff3ac6e97384c2abeb3a697ec9291
GIL <accc45@outlook.com>
2026-04-29 15:47:53 +0900

    Title: 달에서 무게 구하기, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260429/달에서 무게 구하기/weight-on-the-moon.swift
@@ -0,0 +1,7 @@
+import Foundation
+
+let chu: Int = 13
+let gravitation: Double = 0.165
+let product: Double = Double(chu) * Double(gravitation)
+
+print("\(chu) * \(String(format: "%.6f", gravitation)) = \(String(format: "%.6f", product))")
