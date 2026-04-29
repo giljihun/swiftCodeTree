1bf595663301bd6f17f7d290dc3960493e544971
GIL <accc45@outlook.com>
2026-04-29 14:19:12 +0900

    Title: 10개 입력받기, Time: 0ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 260429/10개 입력받기/receive-10-inputs.swift
@@ -0,0 +1,11 @@
+import Foundation
+
+guard let line = readLine() else { exit(0) }
+let nums = line
+    .split(separator: " ")
+    .compactMap { Double($0) }
+    .prefix { $0 != 0 }
+
+print(Int(nums.reduce(0, +)), nums.reduce(0, +) / Double(nums.count))
+
+
