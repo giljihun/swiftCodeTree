3b54e4efe6923def2eea291e0fc8a86042e086dc
GIL <accc45@outlook.com>
2026-04-30 14:14:04 +0900

    Title: 등급 매기기, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260430/등급 매기기/ranking.swift
@@ -0,0 +1,17 @@
+import Foundation
+
+guard let input = readLine(),
+      let score = Int(input) 
+else { exit(0) }
+
+if score >= 90 {
+    print("A")
+} else if score >= 80 {
+    print("B")
+} else if score >= 70 {
+    print("C")
+} else if score >= 60 {
+    print("D")
+} else {
+    print("F")
+} 
